8398489f024b607128cdcf8f2c21a9b584534d22
AVINASH KUMAR <65335071+avipandit@users.noreply.github.com>
2023-04-15 22:24:56 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 import './../styles/App.css';
 
+
+
+
 const App = (props) => {
 
   return (
     <div className="App" id="main">
-      // Do not alter the main div
+    <button id="click">Click me</button>
+  <p id="para" className="hide">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
     </div>
   );
 }
